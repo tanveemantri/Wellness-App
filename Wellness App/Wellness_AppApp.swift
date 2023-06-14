e6048914cc370eb6e0fa29d7c412ac2292a885ca
tanveemantri <tanvee.mantri@gmail.com>
2023-06-14 11:24:30 -0700

    Initial Commit

---
+++ Wellness App/Wellness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wellness_AppApp.swift
+//  Wellness App
+//
+//  Created by Tanvee Mantri on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Wellness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
